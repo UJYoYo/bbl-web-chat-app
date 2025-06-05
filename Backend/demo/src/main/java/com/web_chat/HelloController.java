1df9ae4bf75ac1bafdac4b9724f1f337993e2d49
Non <natthanonprajogo@gmail.com>
2025-06-05 17:52:07 +0700

    got end point /api/hello working

--- Backend/demo/src/main/java/com/web_chat/HelloController.java
+++ Backend/demo/src/main/java/com/web_chat/HelloController.java
@@ -1,10 +1,14 @@
 package com.web_chat;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController 
+@RequestMapping("/api")
 public class HelloController {
-    
-    @RequestMapping("/hello")
+
+    @GetMapping("/hello")
     public String sayHello() {
         return "Hello, World!";
     }
