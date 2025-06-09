3b76d40d9c0fc7c123891ba8fb290369530a771b
Non <natthanonprajogo@gmail.com>
2025-06-09 10:40:42 +0700

    Finished backend end points

---
+++ Backend/demo/src/main/java/com/web_chat/HomeController.java
@@ -0,0 +1,27 @@
+package com.web_chat;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HomeController {
+
+    public static class userBody {
+        public String username;
+        public String password;
+    }
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@RequestBody userBody user) {
+        return "Registering: " + user.username + " with password: " + user.password;
+    }
+    
+}
