cb4d5d04da0def5a78dbe00b54ebb1a558fbe525
Non <natthanonprajogo@gmail.com>
2025-06-11 13:48:10 +0700

    create entity, repository,service and dto folder, updated home controller to try and connect with db

---
+++ backEnd/demo/src/main/java/com/web_chat/service/UserService.java
@@ -0,0 +1,28 @@
+package com.web_chat.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.web_chat.entity.User;
+import com.web_chat.repository.UserRepository;
+
+@Service
+public class UserService {
+    
+    @Autowired
+    private UserRepository userRepository;
+    
+    public User registerUser(String username, String password) {
+        // Check if username already exists
+        if (userRepository.existsByUsername(username)) {
+            throw new RuntimeException("Username already exists");
+        }
+        
+        User user = new User(username, password);
+        return userRepository.save(user);
+    }
+    
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username).orElse(null);
+    }
+}
